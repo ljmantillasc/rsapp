cc64ffe1e4fa780317cf3c8be6f2e9beae92cd28
Luis-Mantilla <luisjosemant@gmail.com>
2022-06-27 11:58:05 -0500

    five commit

--- pages/signin.js
+++ pages/signin.js
@@ -68,14 +68,14 @@ const Home = () => {
             Write a Password
             </label>
             <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************"></input>
-            <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+            <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you d like</p>
             </div>
             <div className="w-full px-3">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
             Write your Password again
             </label>
             <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************"></input>
-            <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+            <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you d like</p>
             </div>
         </div>
         
